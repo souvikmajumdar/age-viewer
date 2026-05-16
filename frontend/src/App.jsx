0a8304f9b025b18bacc8dd8d0751f60e2bc1680a
Souvik Majumdar <souvmaju@ibm.com>
2026-05-15 22:11:38 -0500

    feat: install IBM Carbon, set up global styles and notification system
    Sub-phase 3c-i of Carbon migration.
    
    - Install @carbon/react ^1.107.1 and @carbon/icons-react ^11.80.0
    - Add src/styles/carbon.scss with @use '@carbon/react'
    - Import Carbon styles in App.jsx (coexists with antd/bootstrap)
    - Create NotificationProvider context + useNotification hook
      - Renders Carbon ToastNotification components
      - Exposes success/error/warning/info methods
      - Auto-dismisses after 5 seconds
    - Wrap app in NotificationProvider
    - Update backlog: mark sub-phase 3c-i complete
    
    Build verified: succeeds with Carbon + antd coexisting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,15 +18,19 @@
  */
 
 import React from 'react';
+import './styles/carbon.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
 import './static/style.css';
 import './static/navbar-fixed-left.css';
+import { NotificationProvider } from './hooks/useNotification';
 import MainPage from './pages/Main/MainPage';
 
 const App = () => (
   <React.StrictMode>
-    <MainPage />
+    <NotificationProvider>
+      <MainPage />
+    </NotificationProvider>
   </React.StrictMode>
 );
 
